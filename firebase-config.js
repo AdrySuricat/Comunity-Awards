4e1e1a636dc138e84e050d5412d4849934c2bb24
AdrySuricat <alucavez@gmail.com>
2025-05-31 22:33:42 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+// Firebase config del usuario
+const firebaseConfig = {
+  apiKey: "AIzaSyAujxSBFiHW8BgtjBFQM0a1O9IEJQQV088",
+  authDomain: "comunity-awards.firebaseapp.com",
+  projectId: "comunity-awards",
+  storageBucket: "comunity-awards.firebasestorage.app",
+  messagingSenderId: "593964017438",
+  appId: "1:593964017438:web:41e0c491123e8a1fd28b0f",
+  measurementId: "G-9P736TQ863"
+};
